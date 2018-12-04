06c6751ccb91528d354a3eb03bdb16b7520c0b31
auraizee <auraizee@live.unc.edu>
2018-12-03 21:50:47 -0500

    Update searchscript.js
    Added method to build individual entries for the page
    
    Edited dropdown for better weather options

--- searchscript.js
+++ searchscript.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     function buildSearchInterface() {
         let pageContainer = $('.pageContainer');
         pageContainer.empty();
-        pageContainer.append("<select id='search-type-selector' name='searchChoice'><option value='city'>By City</option><option value='weather'>By Weather</option><option value='flights'>By Number of Flights</option></select>")
+        pageContainer.append("<select id='search-type-selector' name='searchChoice'><option value='city'>By City</option><option value='warmer'>Warmer Than</option><option value='colder'>Colder Than</option></select>")
         pageContainer.append("<input id='search-bar' type='text' placeholder='Search...'>");
         loadEntries();
     }
@@ -35,5 +35,17 @@ $(document).ready(function() {
         });
     }
 
-    
+    function buildEntry(city, state) {
+        let cityName = city;
+        let stateName = state;
+
+        let entry = $('<div class="list-entry"></div>');
+        //append city image to div
+        entry.append('<p class="city-name">' + cityName + '</p>');
+        entry.append('<p class="state-name">' + stateName + '</p>');
+        //append weather data here (user openweathermap API)
+
+        entry.attr("city", cityName);
+        return entry;
+    }
 });
