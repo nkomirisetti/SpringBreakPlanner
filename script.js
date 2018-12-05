dfc62885a7b9a1a8bfc992a9c1607bac9e29f901
Nikhil Komirisetti <nkomirisetti@gmail.com>
2018-12-05 16:08:22 -0500

    removed state parameter because api always returns null anyways

--- script.js
+++ script.js
@@ -95,14 +95,12 @@ $(document).ready(() => {
 
     var venues_url = "https://api.foursquare.com/v2/venues/explore?client_id=1OLLF5IIHTFP0LPT54GNMU1BQHHGONNAZFVDVVFHSB1NPA5G&client_secret=GGIW2UECPUAP23WFAA5LRU2H3I5ZDUY4NJHYDHOVMQUVTHEQ&near="
 
-    var DetailsPage = function (city, state) {
-        city = prompt("What city are we looking at?");
-        state = prompt("What state is this city in?");
+    var DetailsPage = function (city) {
         var pageContainer = $('#pageContainer');
         pageContainer.empty();
 
 
-        var cityName = city + ", " + state;
+        var cityName = city;
         pageContainer.append('<h2>' + cityName + '</h2>');
 
         var returnButton = $('<button class="return">Return</button>');
@@ -114,7 +112,7 @@ $(document).ready(() => {
         var sampleItenDiv = $('<div class="sampleDay" id="sampleDay"></div>');
 
         sampleItenDiv.append('<h3>Lets look at what a day in ' + city + ' could look like: </h3>');
-        $.ajax(venues_url + city + ", " + state + "&query=breakfast&v=20181202&limit=3&radius=30000", {
+        $.ajax(venues_url + city + "&query=breakfast&v=20181202&limit=3&radius=30000", {
             type: 'GET',
             dataType: 'json',
             success: (response) => {
